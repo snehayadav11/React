e0894cd6782436847e20462f4345811267410f9e
sneha <sneha@LAPTOP-JORGPT15.>
2025-01-27 19:19:29 +0530

    Initial commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Chai from "./chai"
+function App() {
+  
+
+  return (
+    <>
+    
+      <h1>Chai aur react | sneha yadav </h1>
+      <Chai />
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+       <h2>Learnig React is intresting</h2> 
+    )
+}
+export default Chai
