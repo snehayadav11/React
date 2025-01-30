7aa5497177226f0944bcf843e4282ba345627506
snehayadav11 <92086602+snehayadav11@users.noreply.github.com>
2025-01-30 18:16:14 +0530

    how variables are injected known as evalvated expression

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,5 +1,5 @@
 
-import Chai from "./chai"
+/*import Chai from "./chai"
 function App() {
   
 
@@ -13,3 +13,28 @@ function App() {
 }
 
 export default App
+
+*/
+
+// variable injection in the above code
+
+import Chai from "./chai"
+function App() {
+  const username = "Sneha Yadav"
+
+  return (
+    <>
+      
+      <h2>Chai aur react username❌ </h2>
+      <h1>Chai aur react {username}✔️</h1>
+      <Chai />
+    </>
+  )
+}// just like in js in string interpolation`${username}` yeh jo curly braces k andar hoga usko bolte h 
+//🚨🚨🚨🚨🚨🚨 interview pov (expression / evalvated expression ) it means final outcome ayga na ki if conditon true h toh or false h toh
+// keeep in  mind
+// ab dekhenge ki ku n likhte yaha p hum if vgera toh for that see ki jb hum react elemnt banate h toh 
+// pele toh type lete h then props then childreen then hum variable inject rte h 
+
+export default App
+
