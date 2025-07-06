ff9c0816dc58f13a53083ca2b2503934dc8bb0ae
snehayadav11 <92086602+snehayadav11@users.noreply.github.com>
2025-07-06 12:54:45 +0530

    component

--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -1,3 +1,4 @@
 // yha p hum apna custom hoook banayene hooks basicallly ek function s ho jo array return krta h with 2 variables 
 // jo ki built in hooks ko bhi use kar skta h like use effect or use state etc 
 // let learn how to make our custom hooks
+
